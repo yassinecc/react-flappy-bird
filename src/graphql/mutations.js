73133227f52b86a816e988268d085702113d55e4
Yassine Chbani <yassine.chbani@gmail.com>
2019-05-25 11:55:58 +0200

    Add API

---
+++ src/graphql/mutations.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createUser = `mutation CreateUser($input: CreateUserInput!) {
+  createUser(input: $input) {
+    id
+    userName
+    value
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    userName
+    value
+  }
+}
+`;
+export const deleteUser = `mutation DeleteUser($input: DeleteUserInput!) {
+  deleteUser(input: $input) {
+    id
+    userName
+    value
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    id
+    userName
+    value
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    userName
+    value
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    userName
+    value
+  }
+}
+`;
